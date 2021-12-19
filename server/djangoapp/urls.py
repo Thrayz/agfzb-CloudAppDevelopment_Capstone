54814b5f14908fc671ecf16c466d4c338f8ad032
Thrayz <traidihamza0@gmail.com>
2021-12-19 11:59:30 +0000

    rest apis

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-path(route='', view=views.get_dealerships, name='index'),
+path(route='', view=views.index, name='index'),
     # path for about view
  path(route='about/', view=views.about, name='about'),
     # path for contact us view
@@ -20,7 +20,7 @@ path(route='login', view=views.login_request, name='login'),
     # path for logout
 path(route='logout', view=views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='dealer_details', view=views.get_dealerships, name='dealer_details'),
 
     # path for dealer reviews view
 path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
